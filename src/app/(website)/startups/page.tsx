2712dd595c0425b70a43f3245496f7ed89b6fde3
James <james@unkey.com>
2026-04-28 08:12:07 -0400

    for a year

--- src/app/(website)/startups/page.tsx
+++ src/app/(website)/startups/page.tsx
@@ -1,13 +1,13 @@
 import { startupsContentData } from '@/constants/startups';
 
 import { getMetadata } from '@/lib/get-metadata';
-import { StartupsApplicationForm } from '@/components/pages/startups/application-form';
 import ProgramSection from '@/components/pages/program/program-section';
+import { StartupsApplicationForm } from '@/components/pages/startups/application-form';
 
 export const metadata = getMetadata({
   title: 'Startups Program',
   description:
-    'Get $1,000 in credits every month for your startup, plus priority support, concierge onboarding, and hands-on migration help. Everyone else gets 50% off their bill.',
+    'Get $1,000 in credits every month for a year for your startup, plus priority support, concierge onboarding, and hands-on migration help. Everyone else gets 50% off their bill.',
   pathname: '/startups',
 });
 
