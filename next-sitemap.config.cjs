8abaf2f0004f7408dd1e925b620c2d9342ca2eff
James <james@unkey.com>
2026-08-11 06:52:19 -0400

    fix error in our commit

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'http://localhost:3000',
+  siteUrl: process.env.NEXT_PUBLIC_DEFAULT_SITE_URL || 'https://www.unkey.com',
   generateRobotsTxt: true,
 };
